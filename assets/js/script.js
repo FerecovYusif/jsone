eadb2594dffffa60366dda6b465b678104f33253
FerecovYusif <plussilvermc@gmail.com>
2024-08-03 09:18:21 +0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+//! Ekrana yazdığımız elementi yazı olaraq göstərmək üçün
+// console.log("Hello world!");
+//! Ekrana yazdığımız elementi error olaraq göstərmək üçün
+// console.error("Hello world!");
+//! Ekrana yazdığımız elementi xəbərdarlıq olaraq göstərmək üçün
+// console.warn("Hello world!");
+
+//? String elementi
+// alert("Suleyman Cakirin kacinci bolumde vuruldu? ");
+
+//? Number elementi
+// console.log(22.04.2004);
+
+//! Ad Soyad Yaş
+// var name = "Suleyman";
+// var surName = "Cakir";
+// var age = 39;
+
+//! Adı Soyadı Yaşı birləşdirmək
+// console.log(name + " " + surName + " " + age);
+
+//! Adı Soyadı Yaşı Istifadəçidən almaq
+var userName = prompt("Suleyman Cakir ilk kimi oldurdu?")
+
+var userLastName = prompt("Suleyman Cakirin yardimcisinin ismi ne?")
+
+var userAge = prompt("Suleyman Cariki kim yetistirdi?")
+
+console.log(userName + " " +  userLastName + " " + userAge);
+
+
+
+
+
